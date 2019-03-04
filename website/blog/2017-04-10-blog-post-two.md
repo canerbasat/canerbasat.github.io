98677099a397aa58ae6d12b48d3c024b684b074e
canerbasat <canerbasat96@gmail.com>
2019-03-04 14:02:49 +0300

    canerbasat

---
+++ website/blog/2017-04-10-blog-post-two.md
@@ -0,0 +1,8 @@
+---
+title: Selenium Ekran Görüntüsü Alma
+author: Blog Author
+authorURL: http://twitter.com/canerbasat
+authorFBID: 100002976521003
+---
+
+Deneme
---
+++ website/core/Footer.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+
+
+
+class Footer extends React.Component {
+  docUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    const docsUrl = this.props.config.docsUrl;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    return `${baseUrl}${docsPart}${langPart}${doc}`;
+  }
+
+  pageUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    return baseUrl + (language ? `${language}/` : '') + doc;
+  }
+
+  
+
+  render() {
+    
+
+    return (
+      
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">
+          <div>
+         
+            
+          </div>
+        </section>
+
+       
+
+
+        <div class="sharethis-inline-follow-buttons"></div>
+
+
+        <section className="copyright">{this.props.config.copyright}
+        <div>
+        <head className="postHeader">
+        <script type='text/javascript' src='//platform-api.sharethis.com/js/sharethis.js#property=5c77cdccdf6421001143afce&product=inline-follow-buttons' async='async'></script>  
+      
+            </head>
+        
+            </div>
+        </section>
+
+
+
+
+
+      </footer>
+    );
+
+
+    
+  }
+}
+
+module.exports = Footer;
